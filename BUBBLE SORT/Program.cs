f11af67ece47d502c853ac04fe6f5535990d601d
khoaitimz <Administrator@KHOAI>
2026-02-09 19:37:47 +0700

    Add project files.

---
+++ BUBBLE SORT/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] array = { 64, 34, 25, 12, 22, 11, 90 };
+        Console.WriteLine("Unsorted array:");
+        PrintArray(array);
+        BubbleSort(array);
+        Console.WriteLine("Sorted array:");
+        PrintArray(array);
+    }
+    private static void BubbleSort(int[] arr)
+    {
+        int n = arr.Length;
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    // Swap arr[j] and arr[j+1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+    private static void PrintArray(int[] arr)
+    {
+        foreach (var item in arr)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine();
+    }
+}
